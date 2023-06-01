810c3de005705a6292b913ab6b1104d941cb5e07
EikiObara <tas30947@gmail.com>
2023-06-02 02:53:32 +0900

    pwaのバグ修正

--- components/meta/HeadInfo.tsx
+++ components/meta/HeadInfo.tsx
@@ -3,7 +3,6 @@ import { SITE_TITLE } from "../../consts";
 const HeadInfo = (): JSX.Element => (
   <>
     <link rel="icon" href="/favicon.ico" />
-    <link rel="manifest" href="/manifest.json" />
     <meta
       name={SITE_TITLE}
       content="Learn how to build a personal website using Next.js"
