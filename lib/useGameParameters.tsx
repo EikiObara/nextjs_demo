20d953dfe9b5c00adfad1b4e246245521c67633f
EikiObara <tas30947@gmail.com>
2023-06-01 23:25:30 +0900

    ボタン類の挙動バグを修正

--- lib/useGameParameters.tsx
+++ lib/useGameParameters.tsx
@@ -17,17 +17,19 @@ export type GameParameter = {
 
 export type GameCallbacks = {
   clickCard: (character: string, index: number) => void;
-  reset: () => void;
+  reset: (question: Question) => void;
 };
 
 const useGameParameters = (
-  question: Question,
+  question: Question
 ): [GameParameter, GameCallbacks] => {
+  // NOTE: states
   const [answerOrder, setAnswerOrder] = useState<string[]>([]);
   const [words, setWords] = useState<string[]>([]);
   const [gameState, setGameState] = useState<GameState>(GAME_STATE.IN_GAME);
   const [isCorrect, setIsCorrect] = useState<GameResult>(GAME_RESULT.IN_GAME);
 
+  // NOTE: User interface
   const clickCard = useCallback(
     (character: string, index: number) => {
       let tempAnswer = answerOrder;
@@ -39,16 +41,20 @@ const useGameParameters = (
 
       setWords([...tempWords]);
     },
-    [answerOrder, setAnswerOrder, setWords]
+    [words, answerOrder, setAnswerOrder, setWords]
   );
 
-  const reset = useCallback(() => {
-    setAnswerOrder([]);
-    setWords(question.text.shuffled);
-    setGameState(GAME_STATE.IN_GAME);
-    setIsCorrect(GAME_RESULT.IN_GAME);
-  }, [question, setAnswerOrder, setWords]);
+  const reset = useCallback(
+    (question: Question) => {
+      setAnswerOrder([]);
+      setWords(question.text.shuffled);
+      setGameState(GAME_STATE.IN_GAME);
+      setIsCorrect(GAME_RESULT.IN_GAME);
+    },
+    [setAnswerOrder, setWords, setGameState, setIsCorrect]
+  );
 
+  // NOTE: auto update process
   useEffect(() => {
     if (question) {
       setWords(question.text.shuffled);
