a2644e95788b32bb2beaa619f758050c276092a8
EikiObara <tas30947@gmail.com>
2023-06-11 21:29:35 +0900

    問題の重複排除処理を削除

--- lib/useQuestion.tsx
+++ lib/useQuestion.tsx
@@ -3,39 +3,20 @@ import { Question } from "./models";
 
 const MAX_FETCH_RETRY = 100;
 
-const isNewQuestion = (pasts: string[], question: Question) => {
-  return pasts.includes(question.text.original);
-};
-
-const fetchQuestion = async (pasts: string[]) => {
-  for (let i = 0; i < MAX_FETCH_RETRY; i++) {
-    const response = await fetch("/api/question");
-    const data: Question = await response.json();
-    if (!isNewQuestion(pasts, data)) {
-      return data;
-    }
-  }
-};
-
 const useQuestion = (): [Question, () => void] => {
   const [question, setQuestion] = useState<Question | undefined>(undefined);
-  const [pasts, setPasts] = useState<string[]>([]);
 
-  const updateQuestion = useCallback(async (pasts: string[]) => {
-    const question = await fetchQuestion(pasts);
+  const updateQuestion = useCallback(async () => {
+    const response = await fetch("/api/question");
 
-    // FIXME: 終わりの挙動を作る
-    if (question) {
-      setQuestion(question);
-      setPasts([...pasts, question.text.original]);
-    }
-  }, []);
+    setQuestion(await response.json());
+  }, [setQuestion]);
 
   useEffect(() => {
-    updateQuestion(pasts);
+    updateQuestion();
   }, [updateQuestion]);
 
-  return [question, () => updateQuestion(pasts)];
+  return [question, () => updateQuestion()];
 };
 
 export default useQuestion;
